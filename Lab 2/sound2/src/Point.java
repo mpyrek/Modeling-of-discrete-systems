3eb884e8c1eafcd23b4a454f38d2ae9a00256cdc
mpyrek <monikapyrek782@gmail.com>
2022-03-09 15:50:57 +0100

    lab2

---
+++ Lab 2/sound2/src/Point.java
@@ -0,0 +1,56 @@
+public class Point {
+
+	public Point nNeighbor;
+	public Point wNeighbor;
+	public Point eNeighbor;
+	public Point sNeighbor;
+	public float nVel;
+	public float eVel;
+	public float wVel;
+	public float sVel;
+	public float pressure;
+	public static Integer []types ={0,1,2};
+	int type;
+	int sinInput;
+
+
+
+	public Point() {
+		for(int i = 0;i<3;i++) types[i]=0;
+		clear();
+	}
+
+	public void clicked() {
+		pressure = 1;
+	}
+	
+	public void clear() {
+		this.nVel = 0;
+		this.eVel = 0;
+		this.wVel = 0;
+		this.sVel = 0;
+		this.pressure = 0;
+	}
+
+	public void updateVelocity() {
+		if(this.type==0) {
+			this.nVel = this.nVel - (this.nNeighbor.pressure - this.pressure);
+			this.wVel = this.wVel - (this.wNeighbor.pressure - this.pressure);
+			this.eVel = this.eVel - (this.eNeighbor.pressure - this.pressure);
+			this.sVel = this.sVel - (this.sNeighbor.pressure - this.pressure);
+		}
+	}
+
+	public void updatePresure() {
+		if(this.type==0) this.pressure = this.pressure - (float) 0.5*(this.nVel+this.wVel+this.eVel+this.sVel);
+		if(this.pressure==2) {
+			double radians = Math.toRadians(sinInput);
+			this.pressure = (float) (Math.sin(radians));
+		}
+
+	}
+
+	public float getPressure() {
+		return pressure;
+	}
+}
